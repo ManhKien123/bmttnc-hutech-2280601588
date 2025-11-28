dda934cae37104eb9086bf2e2bafe9085ae6cb30
dmanhkien1 <dmanhkien1@gmail.com>
2025-11-28 10:40:20 +0700

    [add] lab-01 ex03

---
+++ lab-01/ex03/ex03_03.py
@@ -0,0 +1,10 @@
+def tao_tuple_tu_list(lst):
+    return tuple(lst)
+
+# Nhap danh sach so tu nguoi dung va xu ly chuoi
+input_list = input("Nhập danh sách số, cách nhau bằng dấu cách: ")
+numbers = list(map(int, input_list.split(',')))
+
+my_tuple = tao_tuple_tu_list(numbers)
+print("List: ", numbers)
+print("Tuple từ List: ", my_tuple)  
